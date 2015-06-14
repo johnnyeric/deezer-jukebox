972227b96125b6cc3d9fc552b14ed0c40adfa035
Matthew Hetherington <matt@hue-design.co.uk>
2015-06-14 17:06:37 +0100

    Added UUID generation

--- js/client.js
+++ js/client.js
@@ -1,4 +1,5 @@
 var socket = io();
+var userName = "Matthew Hetherington";
 
 function searchTracks(query) {
     $.ajax({
@@ -20,8 +21,9 @@ function searchTracks(query) {
 
                 for(var i=0; i<response.data.length; i++) {
                     var track = response.data[i];   //ith track from results
-                    var source = track.id;        //Track url
-
+                    track.uuid = $.cookie('dzj-uuid'); //Send our uuid along with the track object
+                    track.userName = userName;
+                    
                     //Add a new <li> element to results
                     $("#results-list").append("<li><button class='topcoat-icon-button'>" +
                                               "<span class='fa fa-plus'></span></button><span class='title'>" + track.title + 
@@ -61,6 +63,17 @@ function updateClientPlayQueue(tracks) {
     }
 }
 
+//From http://stackoverflow.com/a/8809472
+function generateUUID() {
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+}
+
 //When enter is pressed with search bar in focus, search for tracks
 $("#search").keypress(function(e) {
     if(e.which == 13) {
@@ -73,6 +86,13 @@ socket.on('client tracklist', function(tracks){
     updateClientPlayQueue(tracks);
 });
 
-$(window).load(function() {
+$(document).ready(function() {
     socket.emit("request tracklist", null);
+    
+    //Check if we have a UUID set in cookie
+    //Generate one if not
+    
+    if(!$.cookie('dzj-uuid')) {
+        $.cookie('dzj-uuid', generateUUID());
+    }
 });
